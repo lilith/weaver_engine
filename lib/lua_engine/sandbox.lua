e3f0f1892348abd3e1302a6f4ba2ecb8df96d86f
Nathanael Jones <nathanael.jones@gmail.com>
2014-06-20 22:41:11 +0200

    Put most of engine into sandbox.lua (but no error handling, we let ruby deal with any code failures)

--- lib/lua_engine/sandbox.lua
+++ lib/lua_engine/sandbox.lua
@@ -1,4 +1,130 @@
 
+-- host object members
+-- update_stat(k,v) set_stats(stats) 
+-- newpage() print(tab, template) translate(str)
+-- debuglog(str)
+-- add_choice(id,label) set_choices(set)
+
+-- get_mod_blob (id)
+-- get_value_by(key, mod_id = nil, user_id = nil, partition = nil)
+-- set_value_by(key, value, mod_id = nil, user_id = nil, partition = nil) 
+-- flowstack_push(v) flowstack_pop() flowstack_peek()
+
+
+
+-- function push(user_id, mod_id, method_name)
+  --   Success {success=true}
+  --   start_failed (goto or call failed (or module syntax error))
+
+
+
+-- function resume(user_id, mod_id)
+
+  -- Failure reasons {success=false, reason='reason', log=array_of_entries}
+
+  -- empty_stack (new player, or secondary error went unhandled)
+  -- dead_coroutine (abrupt_exit went unhandled)
+  -- runtime_error (an error happened in the module)
+  -- abrupt_exit (module stopped running without saying goodbye)
+  -- infine_goto (infinite loop between modules)
+  -- start_failed (goto or call failed (or module syntax error))
+
+  -- Success {success=true, status="prompt"}
+
+
+function resume(user_id, input)
+  local loop_count = 0
+  local engine_log = {}
+  local err = function(msg)
+    table.insert(engine_log,msg)
+  end
+  repeat
+    -- Resume running the flow at the top of the stack
+    local top_flow = host.flowstack_peek()
+
+    local persist_perms = nil
+    -- if type(top_flow) == "string"...
+    top_flow, persist_perms = sandbox.unpersist(top_flow.binary, user_id, top_flow.mod_id) 
+    if top_flow == nil then
+      return {success=false, reason="empty_stack", log=engine_log}
+    end
+
+    if coroutine.status(top_flow.continuation) == 'dead' then
+      err("Top of stack has dead coroutine: " ..top_flow.description)
+      return {success=false, reason="dead_coroutine", log=engine_log}
+    end
+
+    err("Running flow "..top_flow.description.. " with ".. table.show(input, "input"))
+
+    local was_started, result = coroutine.resume(top_flow.continuation, input)
+    input = nil
+    local is_dead = (coroutine.status(top_flow.continuation) == 'dead') -- Runtime errors or function ended
+    local success = was_started and result and not is_dead
+
+    err((success and "Successful" or "Failed").. " run: was_started=".. tostring(was_started)..
+        ",  dead=" .. tostring(is_dead) .. ",  ".. table.show(result, "result"))
+
+    if not was_started then
+      return {success=false, reason="runtime_error", log=engine_log}
+    end
+    if was_started and is_dead then
+      err("Module exited abruptly!")
+      return {success=false, reason="abrupt_exit", log=engine_log}
+    end
+    -- Prevent endless loops
+    if loop_count > 10 then
+      err("Quitting (10 redirects without user input)")
+      return {success=false, reason="infine_goto", log=engine_log}
+    end
+    loop_count = loop_count + 1
+    
+    -- Handle success
+    if success then
+      host.flowstack_pop()
+      local key = result.status
+      if key == 'prompt' then
+        err("Success, saving updated flow...")
+        local updated_flow = sandbox.persist(top_flow, persist_perms) 
+        host.flowstack_pop()
+        host.flowstack_push(updated_flow)
+        return {success=true, status="prompt"}
+      elseif key == 'donehere' then
+        host.flowstack_pop()
+        err("Module reports completion, popping flow from stack.")
+      elseif key == 'call' or key == 'goto' then
+        if key == 'goto' then
+          err("Module requesting goto, popping flow from stack.")
+          host.flowstack_pop()
+        end
+
+        local push_result = push(user_id,result.mod_id, result.method_name,err) 
+        if not push_result.success then
+          return push_result
+        end
+
+      end
+    end
+      
+  until false
+end
+
+function push(user_id, mod_id, method_name, err)
+  local log = {}
+  if not err then
+    err = function(msg)
+      table.insert(log,msg)
+    end
+  end
+  local new_flow, persist_perms = build_coroutine(user_id, mod_id, method_name, err)
+  if new_flow == nil then
+    return {success=false, reason="start_failed", log=engine_log}
+  else
+    err("New flow pushed to stack")
+    new_flow = sandbox.persist(new_flow, persist_perms)
+    host.flowstack_push({binary=new_flow, mod_id=mod_id})
+    return {success=true}
+  end
+end 
 
 function build_coroutine(user_id, mod_id, method_name, err)
   -- Access the module source
@@ -9,13 +135,7 @@ function build_coroutine(user_id, mod_id, method_name, err)
   end
 
   -- Create the sandboxed environment
-  local env  = get_globals()
-  env.m = sandbox.create_m(user_id,mod_id)
-  env.m.initial_method = method_name
-  env.m.info.id = mod_id
-  env.stats = sandbox.create_stats(user_id)
-  env.require = sandbox.create_require(env)
-
+  local env, persist_perms = sandbox.build_environment(user_id,mod_id)
 
   -- Load the module
   local mod = sandbox.load_with_env(mod_source,env,err)
@@ -38,7 +158,7 @@ function build_coroutine(user_id, mod_id, method_name, err)
 
   local code = coroutine.create(initial_func)
   -- Save the name so they can be recreated
-  return {continuation=code, mod_id =mod_id, method_name=method_name}
+  return {continuation=code, mod_id =mod_id, method_name=method_name, description=mod_id .."#"..method_name}, persist_perms
 end
 
 
@@ -73,12 +193,8 @@ sandbox.env = {
       rad = math.rad, random = math.random, sin = math.sin, sinh = math.sinh, 
       sqrt = math.sqrt, tan = math.tan, tanh = math.tanh },
   os = { clock = os.clock, difftime = os.difftime, time = os.time },
-  debug = {getlocal = debug.getlocal} -- REMOVE THIS
+  --debug = {getlocal = debug.getlocal} -- REMOVE THIS
 }
-
-if host then
-  sandbox.env.newpage = host.newpage
-end
 
 sandbox.create_proxy_access = function(user_id, mod_id)
   local t = {}
@@ -156,22 +272,35 @@ end
 sandbox.persistable = {[math.pi] = math.pi, [math.huge] = math.huge}
 
 -- Creates an environment by copying sandbox.env. Used for sandboxing.
-sandbox.get_globals = function()
-  local globs =  deepcopy(sandbox.env) -- {pairs = pairs,print = print, type=type, coroutine = {yield=coroutine.yield}} -- sandbox_env
-  globs["_G"] = globs
-  return globs
+sandbox.build_environment = function(user_id, mod_id)
+  local env =  deepcopy(sandbox.env)
+  env["_G"] = env  
+  
+  env.m.initial_method = method_name
+  env.m.info.id = mod_id
+
+
+  env.newpage = host.newpage
+  env.checkpoint = host.checkpoint
+  env.m = sandbox.create_m(user_id,mod_id)
+  env.stats = sandbox.create_stats(user_id)
+  env.require = sandbox.create_require(env)
+
+  local err = function(msg)
+    error(msg)
+  end
+  local persist_perms = table.invert(table.flatten_to_functions_array(sandbox.persistable,err))
+  return env
 end
 
-sandbox.get_persist_perms = function()
-  return table.invert(table.flatten_to_functions_array(sandbox.env, sandbox.persistable, print))
+sandbox.unpersist = function(data, user_id,mod_id)
+  local err = function(msg)
+    error(msg)
+  end
+  local perms = table.flatten_to_functions_array(sandbox.build_environment(user_id,mod_id), sandbox.persistable,err)
+
+  returnpluto.unpersist(perms,data)
 end
-
-sandbox.get_unpersist_perms = function ()
-  return table.flatten_to_functions_array(sandbox.env, sandbox.persistable,print )
-end
-
-
-
 
 
 -- table.invert, table.flatten_to_functions_array
