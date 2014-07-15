fcfcda06fa724f5865bc7f4b259a215aec505abb
Nathanael Jones <nathanael.jones@gmail.com>
2014-07-15 18:25:00 -0400

    Get C closures to pass (when run against custom patch of rufus-lua)

--- test/test_c_closure_persist.rb
+++ test/test_c_closure_persist.rb
@@ -21,55 +21,64 @@ describe 'pluto persistence of C closures' do
   end
 
   it 'should fail to persist a C closure' do
-    @s.eval(%{
-      env = {coroutine = {yield=coroutine.yield},hostfunc=host_function}
-      env["_G"] = en
-
-      function routine()
-        coroutine.yield()
-        retval = hostfunc()
-        coroutine.yield(retval) -- "success"
-      end
-
-      setfenv(routine, env)
-      co = coroutine.create(routine)
-      coroutine.resume(co)
-
-      unpersist = {coroutine.yield,hostfunc}
-
-      binary = pluto.persist(table.invert(unpersist),co)
-      co2 = pluto.unpersist(unpersist,binary)
-      local success, result = coroutine.resume(co2)
-      return result
-      }).must_equal "success"
-  end
-
-
-    it 'should not convert a string into a function' do
+    assert_raises Rufus::Lua::LuaError do 
       @s.eval(%{
-        function hf()
-          return host_function()
-        end
-        env = {tostring=tostring,cy = coroutine.yield,hostfunc=hf}
+        env = {coroutine = {yield=coroutine.yield},hostfunc=host_function}
         env["_G"] = env
 
         function routine()
-          local retval = hostfunc()
-          cy(retval)
+          coroutine.yield()
+          retval = hostfunc()
+          coroutine.yield(retval) -- "success"
         end
+
         setfenv(routine, env)
         co = coroutine.create(routine)
-        a, b = coroutine.resume(co)
-        return {a,b}}).to_ruby.must_equal [true,"success"]
+        coroutine.resume(co)
 
+        unpersist = {coroutine.yield,hostfunc}
+
+        binary = pluto.persist(table.invert(unpersist),co)
+        co2 = pluto.unpersist(unpersist,binary)
+        local success, result = coroutine.resume(co2)
+        return result
+        })
+    end 
+  end
+
+
+  it 'should not convert a string into a function when run from a coroutine' do
+    #skip("segfaults")
+    @s.eval(%{
+      
+      function routine()
+        local retval = string.lower(host_function())
+        coroutine.yield(retval)
+      end
+      co = coroutine.create(routine)
+      a, b = coroutine.resume(co)
+      return {a,b}}).to_ruby.must_equal [true,"success"]
+
+  end
+
+  it 'should not convert a string into a function if immediately consumed' do
+    result = @s.eval <<-LUA
+      function routine()
+        local retval = string.lower(host_function())
+        return retval
+      end
+      return routine()
+      LUA
+    result.must_equal "success"
   end
 
 
   it 'should correctly persist a lua closure on a C function' do
+    #skip("segfaults")
     @s.eval(%{
       function hf()
         local retval = host_function()
-        return retval()
+        return retval
       end
       env = {tostring=tostring,cy = coroutine.yield,hostfunc=hf}
       env["_G"] = env
@@ -98,7 +107,7 @@ describe 'pluto persistence of C closures' do
 
     results = [@s.eval("return {a,b}").to_ruby,
                 @s.eval("return {c,d}").to_ruby,
-                @s.eval("return {d,f}").to_ruby]
+                @s.eval("return {e,f}").to_ruby]
 
     results.must_equal [[true,"success"],[true,"success"],[true,"success"]]
   end
