5a3574c27cf3c1fd85398cc98cc234db08a84411
Nathanael Jones <nathanael.jones@gmail.com>
2014-07-23 11:49:26 -0400

    Make FsysDataAdapter less broken.

--- lib/weaver_engine/fsys_data_adapter.rb
+++ lib/weaver_engine/fsys_data_adapter.rb
@@ -11,7 +11,7 @@ module WeaverEngine
 
  
     def get_mod_blob(mod_id)
-      File.read(io.path.join(@module_dir, mod_id, ".lua"))
+      File.binread(File.join(@module_dir, mod_id, ".lua"))
     end
 
 
@@ -29,6 +29,16 @@ module WeaverEngine
       end
     end
 
+    def flowstack_full
+      store = load_store(nil,@user_id,'flowstack')
+      store ? (store[:stack] || []) : []
+    end
+     def flowstack_edit(&block)
+      edit_store(nil,@user_id,'flowstack') do |store|
+        store[:stack]  = block.call(store[:stack] || [])
+        store
+      end
+    end
 
     def flowstack_peek()
       flow = load_store(nil,@user_id,'flowstack')
@@ -43,6 +53,7 @@ module WeaverEngine
         result = store[:stack].pop
         store
       end
+      result
     end
     def flowstack_push(v)
       edit_store(nil,@user_id,'flowstack') do |store|
@@ -53,29 +64,46 @@ module WeaverEngine
       end
     end
 
+   def checkpoints_full
+      store = load_store(nil,@user_id,'checkpoints')
+      store ? (store[:checkpoints] || []): []
+    end
+    def checkpoints_edit(&block)
+      edit_store(nil,@user_id,'checkpoints') do |store|
+        store[:checkpoints] = block.call(store[:checkpoints] || [])
+        store
+      end
+    end
+
+
   
     def calc_path(mod_id = nil, user_id = nil, partition = nil)
-      io.path.join(@data_dir,"#{mod_id || 'allmods'},#{user_id || 'allusers'},#{partition || 'default'},#{branch_id || 'master'}.data")
+      File.join(@data_dir,"#{mod_id || 'allmods'},#{user_id || 'allusers'},#{partition || 'default'},#{branch_id || 'master'}.data")
     end
 
     def load_store(mod_id = nil, user_id = nil, partition = nil)
       fname = calc_path(mod_id,user_id,partition)
       if File.exist?(fname)
-        Marshal.load(File.read())
+        Marshal.load(File.binread(fname))
       else
-        nil
+        {}
       end
     end
 
     def edit_store(mod_id = nil, user_id = nil, partition = nil, &block)
       fname = calc_path(mod_id,user_id,partition)
-      obj = File.exist?(fname) ? Marshal.load(File.read(fname)) : nil
-      obj = block.call(obj)
+      obj = File.exist?(fname) ? Marshal.load(File.binread(fname)) : {}
+      obj = block.call(obj) || {}
       Dir.mkdir(File.dirname(fname)) unless File.directory?(File.dirname(fname))
-      File.open(fname, 'w') {|f| f.write(Marshal.dump(obj)) }
+      File.binwrite(fname, Marshal.dump(obj))
     end
 
     # transaction sources [(user,amount,currency)] destinations [(user,amount,currency)]
+    # For ajax, we eventually would want to diff the tree, i.e compare current against saved.
+    def add_to_state(state,prefix)
+      add_methods_to_state state, prefix, [:update_stat, :set_stats, :newpage, 
+      :print, :translate, :debuglog, :add_choice, :set_choices]
+    end
 
   end
 end
